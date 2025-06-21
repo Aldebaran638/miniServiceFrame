404ed302fe0223316a5dc2f05ab7aee9ba8bc5a7
Aldebaran638 <2023191113@email.szu.edu.cn>
2025-06-21 17:56:40 +0800

    1.编写类UriConfiguration专门用于管理路由配置 2.将原路由函数代码修改，改成采用路由配置类设置路径等而非硬编码（即直接在类中写上路径）

--- gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.miniprogram_frame.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
@@ -17,16 +19,20 @@ public class GatewayApplication {
 	}
 
 	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+		String httpUri = uriConfiguration.getHttpbin();
 		return builder.routes()
 				.route(p -> p
 						.path("/get")
 						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org:80"))
+						.uri(httpUri))
 				.route(p -> p
 						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
-						.uri("http://httpbin.org:80"))
+						.filters(f -> f
+								.circuitBreaker(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")))
+						.uri(httpUri))
 				.build();
 	}
 }
