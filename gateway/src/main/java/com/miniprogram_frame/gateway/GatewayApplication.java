8b0884c55f8cb15dbe74be3389876dc4bde8cd1e
Aldebaran638 <2023191113@email.szu.edu.cn>
2025-06-21 17:12:46 +0800

    初始化网关代码，创建了一个简单的初始路由

---
+++ gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
@@ -0,0 +1,30 @@
+package com.miniprogram_frame.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	/*
+		当向 /get 发起请求时，
+		该路由会将请求转发到 https://httpbin.org/get
+		在我们对这个路由的配置中，我们添加了一个过滤器
+		在请求被转发之前，会向请求中添加一个值为 World 的 Hello 请求头
+	*/
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+			return builder.routes()
+					.route(p -> p
+							.path("/get")
+							.filters(f -> f.addRequestHeader("Hello", "World"))
+							.uri("http://httpbin.org:80"))
+					.build();
+	}
+}
