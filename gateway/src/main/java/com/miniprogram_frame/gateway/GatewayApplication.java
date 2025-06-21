69ea819bc69cec8fb55e20a01f6046b6f26b6692
Aldebaran638 <2023191113@email.szu.edu.cn>
2025-06-21 17:32:34 +0800

    添加路由：只要主机是*.circuitbreaker.com，那么就将请求路由到 HTTPBin，并在请求中添加一个断路器。 HTTPBin：HTTPBin 是一个公共的 HTTP 测试服务，地址是http://httpbin.org。主要用于测试GET、POST、PUT、DELETE 请求是否正确、请求头、请求参数是否正确和响应格式是什么样子。 断路器：当下游服务（比如 Oat++ 服务）出现超时、服务宕机或者返回大量500错误时，网关就会临时拒绝请求，直接返回 fallback 响应，不再等待下游服务恢复。

--- gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -12,19 +15,24 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
 	/*
-		当向 /get 发起请求时，
-		该路由会将请求转发到 https://httpbin.org/get
-		在我们对这个路由的配置中，我们添加了一个过滤器
-		在请求被转发之前，会向请求中添加一个值为 World 的 Hello 请求头
-	*/
+	 * 当向 /get 发起请求时，
+	 * 该路由会将请求转发到 https://httpbin.org/get
+	 * 在我们对这个路由的配置中，我们添加了一个过滤器
+	 * 在请求被转发之前，会向请求中添加一个值为 World 的 Hello 请求头
+	 */
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-			return builder.routes()
-					.route(p -> p
-							.path("/get")
-							.filters(f -> f.addRequestHeader("Hello", "World"))
-							.uri("http://httpbin.org:80"))
-					.build();
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p
+						.host("*.circuitbreaker.com")
+						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+						.uri("http://httpbin.org:80"))
+				.build();
 	}
 }
