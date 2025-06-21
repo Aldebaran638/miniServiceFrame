e18f939a7002e87e903ce6dcc9d843852fe2c4e8
Aldebaran638 <2023191113@email.szu.edu.cn>
2025-06-21 17:45:00 +0800

    1.当主机是*.circuitbreaker.com时，路由到HTTPBin的请求超时时，会返回/fallback路径的文本结果
    2.添加了 /fallback路径 的返回文本结果

--- gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/miniprogram_frame/gateway/GatewayApplication.java
@@ -25,7 +25,7 @@ public class GatewayApplication {
 						.uri("http://httpbin.org:80"))
 				.route(p -> p
 						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
 						.uri("http://httpbin.org:80"))
 				.build();
 	}
